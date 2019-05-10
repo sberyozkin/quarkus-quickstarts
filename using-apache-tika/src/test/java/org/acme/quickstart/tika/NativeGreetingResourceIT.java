242a6283c156f4ab6744b1ac816256a21455a473
Sergey Beryozkin <sberyozkin@gmail.com>
2019-05-10 16:49:07 +0100

    Enabling the pdf parser native test

--- using-apache-tika/src/test/java/org/acme/quickstart/tika/NativeGreetingResourceIT.java
+++ using-apache-tika/src/test/java/org/acme/quickstart/tika/NativeGreetingResourceIT.java
@@ -1,15 +1,7 @@
 package org.acme.quickstart.tika;
 
 import io.quarkus.test.junit.SubstrateTest;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 
 @SubstrateTest
 public class NativeGreetingResourceIT extends GreetingResourceTest {
-
-    @Test
-    @Disabled
-    @Override
-    public void testHelloQuarkusPdfFormat() throws Exception {
-    }
 }
